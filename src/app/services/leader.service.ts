a3d62f349fd71c62e94c984b7045d983a544d708
LU1ZC4RL05 <luizmk_rulez@live.com>
2020-08-05 21:35:11 -0300

    Assignment 2.1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -9,6 +9,9 @@ export class LeaderService {
   getLeaders(): Leader[] {
     return LEADERS;
   }
+  getLeader(id : number):Leader {
+    return LEADERS.filter((leader) => leader.id == id)[0];
+  }
   getFeaturedLeader(): Leader {
     return LEADERS.filter((leader) => leader.featured)[0];
   }
