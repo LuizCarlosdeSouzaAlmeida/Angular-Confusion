468a5240224c08aa1fffb9a795b9672119da2f0c
LuizCarlosdeSouzaAlmeida <luizmk_rulez@live.com>
2022-01-09 20:14:13 -0300

    Saving Changes to Server

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -34,6 +34,7 @@ export class DishdetailComponent implements OnInit {
   };
 
   dish: Dish;
+  dishcopy: Dish;
   dishIds: string[];
   prev: string;
   next: string;
@@ -56,14 +57,10 @@ export class DishdetailComponent implements OnInit {
       .getDishIds()
       .subscribe(dishIds => this.dishIds = dishIds,
         errmess => this.errMess = <any>errmess);
-    this.route.params
-      .pipe(
-        switchMap((params: Params) => this.dishService.getDish(params['id']))
-      )
-      .subscribe((dish) => {
-        this.dish = dish;
-        this.setPrevNext(dish.id);
-      });
+        this.route.params
+        .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+          errmess => this.errMess = <any>errmess );
   }
 
   setPrevNext(dishId: string) {
@@ -130,6 +127,12 @@ export class DishdetailComponent implements OnInit {
     this.comment = this.commentForm.value;
     this.comment.date = new Date().toISOString();
     this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishService.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish;
+      },
+      errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; });
     this.commentForm.reset({
       rating: 5,
       comment: '',
