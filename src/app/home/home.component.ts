1e1dfe9216bcf91d45fe0b2e33d23ffc3590a5df
LuizCarlosdeSouzaAlmeida <luizmk_rulez@live.com>
2022-01-09 21:15:12 -0300

    Animations Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,21 @@ import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+    // tslint:disable-next-line:use-host-property-decorator
+    host: {
+      '[@flyInOut]': 'true',
+      'style': 'display: block;'
+      },
+      animations: [
+        flyInOut(),
+        expand()
+      ]
 })
 export class HomeComponent implements OnInit {
   dish: Dish;
